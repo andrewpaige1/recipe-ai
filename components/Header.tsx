a0e766559543e26ff9a5fbd8082a0061f1f3499e
Andrew Paige <keeperpaige@gmail.com>
2024-09-11 23:43:03 -0400

    update: remove logo divider

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,6 @@
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <span className="border-l rotate-45 h-6" />
-      </div>
       <div className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
         <p
           className="font-bold hover:underline"
